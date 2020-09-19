fe637331429e7c4487d75f7ecc627cc41c3c3375
StandOutstar <zxk.albert@foxmail.com>
2020-09-19 18:01:34 +0800

    feat(CronJobService): 切换到 System.Timer.Timer

--- HostedServiceDemo/HostedServices/CronJobs/CronJobService.cs
+++ HostedServiceDemo/HostedServices/CronJobs/CronJobService.cs
@@ -1,17 +1,17 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using Cronos;
 using Microsoft.Extensions.Hosting;
-// using Timer = System.Timers.Timer;
-using Timer = System.Threading.Timer;
+using Timer = System.Timers.Timer;
+//using Timer = System.Threading.Timer;
 
 namespace HostedServiceDemo.HostedServices.CronJobs
 {
     public abstract class CronJobService: IHostedService, IDisposable
     {
         private Timer _timer;
-        private int executionCount = 0;
         private readonly CronExpression _cronExpression;
         private readonly TimeZoneInfo _timeZoneInfo;
 
@@ -25,56 +25,43 @@ namespace HostedServiceDemo.HostedServices.CronJobs
         {
             await ScheduleJob(cancellationToken);
         }
-        
-        // protected virtual async Task ScheduleJob(CancellationToken cancellationToken)
-        // {
-        //     var next = _cronExpression.GetNextOccurrence(DateTimeOffset.Now, _timeZoneInfo);
-        //     if (next.HasValue)
-        //     {
-        //         var delay = next.Value - DateTimeOffset.Now;
-        //         if (delay.TotalMilliseconds <= 0)   // prevent non-positive values from being passed into Timer
-        //         {
-        //             await ScheduleJob(cancellationToken);
-        //         }
-        //         else
-        //         {
-        //             Console.WriteLine($"schedule next  {this} job delay: {delay.TotalSeconds}");
-        //             _timer = new Timer(delay.TotalMilliseconds); 
-        //             _timer.Elapsed += async (sender, args) =>
-        //             {
-        //                 _timer.Dispose();  // reset and dispose timer
-        //                 _timer = null;
-        //
-        //                 if (!cancellationToken.IsCancellationRequested)
-        //                 {
-        //                     await DoWork(cancellationToken);
-        //                 }
-        //
-        //                 if (!cancellationToken.IsCancellationRequested)
-        //                 {
-        //                     await ScheduleJob(cancellationToken);    // reschedule next
-        //                 }
-        //             };
-        //             _timer.Start();
-        //         }
-        //     }
-        //     await Task.CompletedTask;
-        // }
 
         protected virtual async Task ScheduleJob(CancellationToken cancellationToken)
         {
-            var interval = _cronExpression.GetNextOccurrence(DateTimeOffset.Now, _timeZoneInfo);
-            _timer = new Timer(new TimerCallback(TestDo), null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+            var next = _cronExpression.GetNextOccurrence(DateTimeOffset.Now, _timeZoneInfo);
+            if (next.HasValue)
+            {
+                var delay = next.Value - DateTimeOffset.Now;
+                if (delay.TotalMilliseconds <= 0)   // prevent non-positive values from being passed into timer
+                {
+                    Console.WriteLine($"{this} at: {DateTimeOffset.Now} delay: {delay}");
+                    await ScheduleJob(cancellationToken);
+                }
+                else
+                {
+                    Console.WriteLine($"schedule next {this} job delay: {delay.TotalMilliseconds}");
+                    _timer = new Timer(delay.TotalMilliseconds);
+                    _timer.Elapsed += async (sender, args) =>
+                    {
+                        _timer.Dispose();  // reset and dispose timer
+                         _timer = null;
+
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            await DoWork(cancellationToken);
+                        }
+
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            await ScheduleJob(cancellationToken);    // reschedule next
+                        }
+                    };
+                    _timer.Start();
+                }
+            }
             await Task.CompletedTask;
         }
 
-        private void TestDo(object state)
-        {
-            var count = Interlocked.Increment(ref executionCount);
-
-            Console.WriteLine(
-                $"{DateTime.Now:hh:mm:ss.fff} Timed Hosted Service ({this}) is working. Count: {count}");
-        }
         
         public virtual async Task DoWork(CancellationToken cancellationToken)
         {
@@ -83,8 +70,8 @@ namespace HostedServiceDemo.HostedServices.CronJobs
 
         public virtual async Task StopAsync(CancellationToken cancellationToken)
         {
-            // _timer?.Stop();
-            _timer?.Change(Timeout.Infinite, 0);
+            _timer?.Stop();
+            //_timer?.Change(Timeout.Infinite, 0);
             await Task.CompletedTask;
         }
 
